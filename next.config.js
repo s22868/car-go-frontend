d3cd7611646770d2b4938354c0502061e46cff80
Mati <mati.leeder@gmail.com>
2023-01-03 17:43:42 +0100

    chore: update next config for testing

--- next.config.js
+++ next.config.js
@@ -1,4 +1,14 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'tesla-cdn.thron.com',
+        port: '',
+        pathname: '/**/*',
+      },
+    ],
+  },
   reactStrictMode: true,
 }
