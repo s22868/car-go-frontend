6f3d533aaac348d526f7166e58d1ce6254f2cd0a
Mati <mati.leeder@gmail.com>
2023-01-03 14:55:09 +0100

    chore: create brand colors theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -6,7 +6,17 @@ module.exports = {
     './app/**/*.{js,ts,jsx,tsx}',
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'brand-gray': {
+          100: '#E9E9E9',
+          200: '#B7B7B7',
+          300: '#232323',
+          400: '#222222',
+        },
+        'brand-red': '#FF494B',
+      },
+    },
   },
   plugins: [],
 }
