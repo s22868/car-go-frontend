4b1759f9a9847f86035f3cdc49bc50efb33dd8fc
Mati <mati.leeder@gmail.com>
2023-01-09 00:45:59 +0100

    feat: add select component

---
+++ components/shared-components/select/Select.tsx
@@ -0,0 +1,16 @@
+import React, { ComponentPropsWithoutRef, FC } from 'react'
+
+interface SelectProps extends ComponentPropsWithoutRef<'select'> {}
+
+const Select: FC<SelectProps> = ({ children, ...props }) => {
+  return (
+    <select
+      {...props}
+      className="block px-6 font-medium placeholder-gray-400 outline-none rounded-2xl h-14 bg-brand-gray-300 text-brand-gray-200 focus:ring-blue-500 focus:border-blue-500"
+    >
+      {children}
+    </select>
+  )
+}
+
+export default Select
