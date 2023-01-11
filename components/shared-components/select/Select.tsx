87a44b6979ab16cdfe8b35fc8150e86f3b3b4c92
Mati <mati.leeder@gmail.com>
2023-01-11 23:51:52 +0100

    style: update create-offer page

--- components/shared-components/select/Select.tsx
+++ components/shared-components/select/Select.tsx
@@ -6,7 +6,7 @@ const Select: FC<SelectProps> = ({ children, ...props }) => {
   return (
     <select
       {...props}
-      className="block px-6 font-medium placeholder-gray-400 outline-none rounded-2xl h-14 bg-brand-gray-300 text-brand-gray-200 focus:ring-blue-500 focus:border-blue-500"
+      className="block px-6 font-medium placeholder-gray-400 outline-none rounded-2xl h-14 bg-brand-gray-400 text-brand-gray-200 focus:ring-blue-500 focus:border-blue-500"
     >
       {children}
     </select>
