6caa27bd399f0f8f33e03a6ceb1d6b27b5c3834b
Mati <mati.leeder@gmail.com>
2023-01-15 15:56:55 +0100

    feat: add icons

---
+++ components/shared-components/icons/CardChip.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function CardChip() {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="48"
+      height="44"
+      fill="none"
+      viewBox="0 0 48 44"
+    >
+      <g fill="#B7B7B7" fillRule="evenodd" clipRule="evenodd" opacity="0.3">
+        <path d="M0 35.804C0 40.06 3.46 43.5 7.732 43.5h8.192v-8.61H0v.914zM0 32.155h15.92v-8.787H0v8.787zM0 8.2v.92h15.925V.5H7.732C3.46.5 0 3.949 0 8.2zM40.267.5h-21.6v43h10.985V9.12H48V8.2c0-4.251-3.465-7.7-7.732-7.7zM0 20.636h15.92V11.85H0v8.786z"></path>
+        <path d="M32.398 20.636H48V11.85H32.398v8.786zM32.398 32.155H48v-8.787H32.398v8.787zM32.398 43.5h7.87c4.267 0 7.732-3.44 7.732-7.696v-.914H32.398v8.61z"></path>
+      </g>
+    </svg>
+  )
+}
+
+export default CardChip
---
+++ components/shared-components/icons/XIcon.tsx
@@ -0,0 +1,23 @@
+import React, { FC } from 'react'
+
+const XIcon: FC = () => {
+  return (
+    <svg
+      xmlns="http://www.w3.org/2000/svg"
+      width="42"
+      height="42"
+      fill="none"
+      viewBox="0 0 42 42"
+    >
+      <circle cx="21" cy="21" r="21" fill="#FF494B"></circle>
+      <path
+        fill="#E9E9E9"
+        fillRule="evenodd"
+        d="M15.487 17.339a1 1 0 011.401-1.428l3.875 3.804 3.875-3.804a1 1 0 011.401 1.428l-4.053 3.978 4.057 4.023a1 1 0 11-1.409 1.42l-3.871-3.84-3.871 3.84a1 1 0 01-1.409-1.42l4.057-4.023-4.053-3.978z"
+        clipRule="evenodd"
+      ></path>
+    </svg>
+  )
+}
+
+export default XIcon
